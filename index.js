c612a602ed38c6c76cf3df187fe8927292459762
Chase <chaseit12@gmail.com>
2018-04-01 20:51:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Code your solution here:
+
+
+function driversWithRevenueOver(drivers_array_with_objects, revenue){
+  return drivers_array_with_objects.filter((each_object)=>{
+    return drivers_array_with_objects.revenue > revenue
+  })
+}
